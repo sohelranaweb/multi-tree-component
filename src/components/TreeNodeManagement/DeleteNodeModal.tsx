508d264d143851a5512e72ed97466f550f3c9df6
sohelranaweb <sohel.rana.web2@gmail.com>
2025-12-16 16:57:20 +0600

    Node Delete functionality complete

---
+++ src/components/TreeNodeManagement/DeleteNodeModal.tsx
@@ -0,0 +1,37 @@
+import { Button } from "../ui/button";
+
+interface DeleteModalProps {
+  onClose: () => void;
+  onConfirm: () => void;
+  nodeName: string;
+}
+function DeleteNodeModal({ onClose, onConfirm, nodeName }: DeleteModalProps) {
+  return (
+    <div
+      className="fixed inset-0 bg-black/50 flex items-center justify-center z-50"
+      onClick={onClose}
+    >
+      <div
+        className="bg-gray-800 rounded-lg p-6 w-96 "
+        onClick={(e) => e.stopPropagation()}
+      >
+        <h2 className="text-4xl font-semibold text-white mb-4">
+          Confirm Delete
+        </h2>
+        <p>
+          Are you sure you want to delete "{nodeName}" and all its children?
+        </p>
+        <div className="flex gap-2 justify-end mt-4">
+          <Button onClick={onClose} variant="secondary">
+            Cancel
+          </Button>
+          <Button onClick={onConfirm} variant="destructive">
+            Delete
+          </Button>
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default DeleteNodeModal;
